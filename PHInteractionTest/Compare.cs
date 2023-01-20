7878382a985025cb4f05c668f433f1933567177a
patelshivani3 <shivpatel3001@gmail.com>
2023-01-20 17:45:04 +0530

    [Shivani Patel]Adding Generic Program

---
+++ PHInteractionTest/Compare.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PHInteractionTest
+{
+    public class Compare<T> where T : IComparable
+    {
+        public T num1, num2, num3;
+        public T[] value;
+
+        // Parameterized Constructor
+        public Compare(T num1, T num2, T num3)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+            this.num3 = num3;
+        }
+
+        //Creating method to find maximum  by using Generic method
+        public static T MaxNumber(T num1, T num2, T num3)
+        {
+
+            if ((num1.CompareTo(num2) > 0) && (num1.CompareTo(num3) > 0))
+            {
+                return num1;
+            }
+            else if ((num2.CompareTo(num1) > 0) && (num2.CompareTo(num3) > 0))
+            {
+                return num2;
+            }
+            else
+            {
+                return num3;
+            }
+        }
+    }
+}
---
+++ PHInteractionTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PHInteractionTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nwelcome to generic");
+            //Integer
+            Console.WriteLine("Enter Any 3 Integer Numbers:");
+            int num1Int = Convert.ToInt32(Console.ReadLine());
+            int num2Int = Convert.ToInt32(Console.ReadLine());
+            int num3Int = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Max Value:" + Compare<int>.MaxNumber(num1Int, num2Int, num3Int));
+            Console.WriteLine("-----------------------------------------------------");
+            // Float
+            Console.WriteLine("Enter Any 3 Float Numbers:");
+            float num1Float = Convert.ToSingle(Console.ReadLine());
+            float num2Float = Convert.ToSingle(Console.ReadLine());
+            float num3Float = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Max Value:" + Compare<float>.MaxNumber(num1Float, num2Float, num3Float));
+            Console.WriteLine("-----------------------------------------------------");
+            // String
+            Console.WriteLine("Enter Any 3 Strings:");
+            string num1String = Convert.ToString(Console.ReadLine());
+            string num2String = Convert.ToString(Console.ReadLine());
+            string num3String = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Max Value:" + Compare<string>.MaxNumber(num1String, num2String, num3String));
+            Console.ReadLine();
+        }
+    }
+}
